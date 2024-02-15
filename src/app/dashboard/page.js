55b88370b634a084520877c3233c19471783bac0
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-02-15 12:28:40 +0530

    base folder structure

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '@/components/dashboard/sidebar'
+
+const page = () => {
+  return (
+    <div>
+
+<Sidebar/>
+
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/signup/page.js
@@ -0,0 +1,11 @@
+
+import Signup from "../../components/signup/page"
+const page = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default page
